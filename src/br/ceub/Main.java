935618993c85b162ac24325df6e515526fd2362d
Gustavo Ramos de Azevedo <gustavoramos.zvd@gmail.com>
2026-03-28 21:11:23 -0300

    Delete src/br/ceub/Main.java

--- src/br/ceub/Main.java
+++
@@ -1,8 +0,0 @@
-package br.ceub;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("===== SISTEMA DE GERENCIAMENTO DE BIBLIOTECA =====\n");
-        
-        
-    }
-}
